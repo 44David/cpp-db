30a2f0bea8c22a9fa4368b34f6df5589d88a0c3b
David Soane <david.soane@outlook.com>
2024-07-19 19:09:31 -0600

    added a vector for storing text data

--- main.cpp
+++ main.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 class Table {
     public:
-        int ID;
+        std::string tableName;
+        std::string ID;
         std::string username;
 };
 
-int read_query() {
+void read_query() {
 
-    std::ifstream db_query("db-query.txt");
+        std::vector<std::string> table_vector;
 
-    std::string line;
+        std::ifstream db_query("db-query.txt");
 
-    while (getline (db_query, line)) {
-        std::cout << line << std::endl;
-    };
-    db_query.close();
+        std::string line;
 
+        while (getline (db_query, line)) {
+            table_vector.push_back(line);
+        };
+        db_query.close();
+
+        // for (size_t i  = 0; i < table_vector.size(); i++) {
+        //     std::cout << table_vector[i] << " , ";
+        // } std::cout << std::endl;
+
+
+        Table data_table;
+
+        data_table.tableName = table_vector[0];
+        data_table.ID = table_vector[1];
+        data_table.username = table_vector[2];
+
+        std::cout << data_table.tableName;
+
+
+    
 };
 
 int main() {
     read_query();
-
+    
+    
 }
 
