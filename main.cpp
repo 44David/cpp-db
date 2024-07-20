ad138bc403fdce1fe0d540ec0fa167a7f2675592
David Soane <david.soane@outlook.com>
2024-07-19 19:30:04 -0600

    added timestamp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <ctime>
 
 class Table {
     public:
         std::string tableName;
         std::string ID;
         std::string username;
+        time_t createdAt;
 };
 
 void read_query() {
@@ -25,27 +27,26 @@ void read_query() {
 
         Table data_table;
 
+        time_t outputAt;
+        time(&outputAt);
+
         data_table.tableName = table_vector[0];
         data_table.ID = table_vector[1];
         data_table.username = table_vector[2];
 
         std::cout << data_table.tableName;
 
-
-
         std::ofstream output_table("output-table.txt");
-        for (size_t i  = 0; i < table_vector.size(); i++) {
+
+        output_table << "TABLE " << data_table.tableName << " {" << std::endl;
+        for (size_t i  = 1; i < table_vector.size(); i++) {
             output_table << table_vector[i] << std::endl;
         };
-
-
-
-    
+        output_table << "}";
+        output_table.close();
 };
 
 int main() {
     read_query();
-    
-    
 }
 
